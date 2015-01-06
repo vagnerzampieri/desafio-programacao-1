4e8f84cda88d19af83cfdb24fae06df6ab4a01d0
Vagner Zampieri <vgzampieri@gmail.com>
2015-01-06 21:44:12 -0200

    Create document in controller

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -3,4 +3,21 @@ class DocumentsController < ApplicationController
     @document = Document.new
     @sales    = Sale.all
   end
+
+  def create
+    @document = Document.new(parameters)
+    if @document.save
+      Sale.importing(@document.file.path)
+      redirect_to root_path, notice: 'Salvo!'
+    else
+      redirect_to root_path, alert: 'Documento inválido'
+    end
+  end
+
+  private
+
+  def parameters
+    params[:document] = {file: ''} unless params[:document].present?
+    params.require(:document).permit(:file)
+  end
 end
---
+++ spec/controllers/documents/create_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe DocumentsController do
+  describe 'POST create' do
+    context 'with valid attributes' do
+      before do
+        filepath = Rack::Test::UploadedFile.new([Rails.root, 'example_input.tab'].join('/'))
+        post :create, { document: { file: filepath } }
+      end
+
+      it { is_expected.to respond_with(302) }
+      it { expect(response).to redirect_to(root_path) }
+      it { expect(Document.count).to eql(1) }
+      it { expect(Sale.count).to eql(4) }
+    end
+
+    context 'with invalid attributes' do
+      before do
+        post :create, { document: { file: '' } }
+      end
+
+      it 'redirect back' do
+        expect(response).to redirect_to(root_path)
+      end
+
+      it 'Document count 0' do
+        expect(Document.count).to eql(0)
+      end
+
+      it 'Sale count 0' do
+        expect(Sale.count).to eql(0)
+      end
+    end
+  end
+end
