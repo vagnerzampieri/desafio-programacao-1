a795dd18568b30c27984d85206444ce8f5eae024
Vagner Zampieri <vgzampieri@gmail.com>
2015-01-06 18:38:22 -0200

    Create table documents

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150106194721) do
+ActiveRecord::Schema.define(version: 20150106202705) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "documents", force: :cascade do |t|
+    t.string   "file"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "sales", force: :cascade do |t|
     t.string   "purchaser_name"
