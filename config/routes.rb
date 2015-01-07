8e2fdb516fce4ebec23f718f454e49f8d437784d
Vagner Zampieri <vgzampieri@gmail.com>
2015-01-07 05:54:06 -0200

    Adding controller users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root 'documents#index'
   post 'documents', to: 'documents#create', as: :documents
+
+  get "users/new"
+  get "/auth/:provider/callback" => "sessions#create"
+  get "/logout" => "sessions#destroy", :as => :logout
 end
