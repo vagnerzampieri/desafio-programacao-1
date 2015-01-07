341159d9c77211bd2bf2059174d579858aa87b33
Vagner Zampieri <vgzampieri@gmail.com>
2015-01-07 06:58:03 -0200

    Adding find_or_create_by

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ActiveRecord::Base
+  def self.find_or_create_omniauth(auth)
+    find_or_create_by(
+      provider: auth['provider'],
+      uid:      auth['uid'],
+      name:     auth['info']['name']
+    )
+  end
 end
